ce525d2002dc41db055df6c006ff20d9ed8cf7cd
021598111 <1073475832@qq.com>
2025-01-31 18:32:45 +0900

    Homework 4

--- 2024_fall/lec04/homework4.py
+++ 2024_fall/lec04/homework4.py
@@ -1,12 +1,14 @@
-
+     
 def list_to_dict(input_list):
     '''
     This function should return a dictionary in which each element of 
     `input_list` is a value, and the corresponding key is the numerical 
     index of that element in `input_list`. 
     '''
-    pass
-
+    newdict={ }
+    for n,v  in enumerate( imput-list)
+                        newdict[n]=v
+    return newdict
 
 
 
